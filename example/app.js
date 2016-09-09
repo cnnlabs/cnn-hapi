85954fa68fde8a56e4779fa6916515daec0ec60e
Brian Duckett <brian.duckett@turner.com>
2016-09-09 16:17:08 -0400

    replacing app.js with server.js

--- example/app.js
+++ example/app.js
@@ -1,49 +1,54 @@
 'use strict';
 
-/*
- * Example application that uses cnn-hapi as a dependency to provide a basic
- * Hapi server with built in features that this example doesn't need to care
- * about.
- *
- * See the comments inline for changes that would be typical in an external app
- */
+
 const path = require('path'),
-    hapi = require('../main'), // hapi = require('cnn-hapi'),
+    hapi = require('../init'), // hapi = require('cnn-hapi'),
     cnnhealth = require('cnn-health'),
     otherChecks = require('./config/otherchecks');
 
+
+
 let healthChecks = cnnhealth(path.resolve(__dirname, './config/healthcheck')).asArray(),
-    app;
-
-healthChecks = healthChecks.concat(otherChecks);
-
-app = module.exports = hapi({
-    directory: __dirname,
-    port: process.env.PORT,
-    name: 'testHarness',
-    description: 'A Test Harness for building CNN-HAPI',
-    withSwagger: true,
-    // metrics: {provider: require('cnn-metrics'), options: {flushEvery: 20 * 1000}},
-    layoutsDir: `${__dirname}/views/`,
-    healthChecks: healthChecks,
-    maxAge: '10',
-    surrogateCacheControl: 'max-age=60, stale-while-revalidate=10, stale-if-error=6400',
-    customHeaders: [
-        {
+    app,
+    server = hapi.instance({
+        basePath: __dirname,
+        customHeaders: [{
             name: 'Connection',
             value: 'close'
-        }
-    ]
-});
+        }],
+        description: 'A Test Harness for building CNN-HAPI',
+        envProd: 'prod',
+        healthChecks: healthChecks.concat(otherChecks),
+        layoutsDir: 'views',
+        maxAge: '10',
+        maxListeners: 1000,
+        name: 'testHarness',
+        port: process.env.PORT,
+        surrogateCacheControl: 'max-age=60, stale-while-revalidate=10, stale-if-error=6400',
+        withSwagger: true,
+        withGoodConsole: true
+    });
 
-app.route({
-    method: 'GET',
-    path: '/',
-    handler: function routeHandler(request, reply) {
-        reply('Hello router');
+
+
+/* get the default state of the server */
+app = server.hapi;
+
+
+
+/* load plugins registered by the core pkg */
+app.register(server.pkgRegistry, (error) => {
+    if (error) {
+        console.error(error); process.exit(1);
     }
 });
 
-app.start(function () {
-    console.log('App Starting');
+
+/* set the application routes */
+app.route(require('./routes'));
+
+
+
+app.start(function serverStart() {
+    console.log('info', `Server running at ${app.info.uri}`);
 });
