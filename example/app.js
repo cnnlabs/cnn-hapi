13ffe3b0e5dd955d45762ffaf854d962a3c9db8d
Jenny Vallon <jennyvallon@gmail.com>
2018-04-20 12:25:41 -0400

    .

--- example/app.js
+++ example/app.js
@@ -11,38 +11,7 @@ const {resolve} = require('path');
 const hapi = require('../init'); // hapi = require('cnn-hapi'),
 const healthChecks = require('cnn-health')(resolve(__dirname, './config/healthcheck')).asArray();
 
-const server = hapi({
-  basePath: __dirname,
-
-  description: 'A Test Harness for building CNN-HAPI',
-  name: 'testHarness',
-  surrogateCacheControl: 'max-age=66, stale-while-revalidate=10, stale-if-error=6400',
-
-  loaderIoValidationKey: process.env.LOADER_IO_VALIDATION,
-  port: process.env.PORT,
-
-  withGoodConsole: true,
-  withSwagger: true,
-  host: 'localhost',
-
-  healthChecks: [...healthChecks, require('./config/otherchecks')],
-
-  customHeaders: [
-    {
-      name: 'Connection',
-      value: 'close'
-    }
-  ],
-  localTLS: {
-    cert: '.local/localhost.cnn.io.crt',
-    key: '.local/localhost.cnn.io.key',
-    port: process.env.LOCAL_TLS_PORT
-  },
-  metrics: {
-    provider: require('cnn-metrics'),
-    options: {flushEvery: 6 * 1000}
-  }
-});
+const server = hapi();
 
 /* get the hapi server */
 const app = server.hapi;
